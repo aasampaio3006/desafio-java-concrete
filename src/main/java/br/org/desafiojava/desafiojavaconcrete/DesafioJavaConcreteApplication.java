ed3f1732f727537675efeca5a03b5d11717f418b
Andrade Sampaio <andrade.sampaio@outlook.com>
2019-05-21 18:45:26 -0300

    Initial commit

---
+++ src/main/java/br/org/desafiojava/desafiojavaconcrete/DesafioJavaConcreteApplication.java
@@ -0,0 +1,13 @@
+package br.org.desafiojava.desafiojavaconcrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaConcreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaConcreteApplication.class, args);
+	}
+
+}
